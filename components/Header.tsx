b4c0bcf157f3cfafb7ac3eec5844fb926fcbc24b
Sergio Hernandez <sergiomanueel@gmail.com>
2023-06-26 21:43:17 -0400

    auth modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,15 @@ import { RxCaretLeft } from "react-icons/rx";
 import { RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast"
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
 
 import Button from "./Button";
+
 
 interface HeaderProps {
     children: React.ReactNode
@@ -18,10 +25,22 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter();
 
-    const handleLogout = () => {
-        // handle logout
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        // TODO: reset any playing songs
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success('Logged Out!')
+        }
     }
 
     return ( 
@@ -68,30 +87,47 @@ const Header: React.FC<HeaderProps> = ({
                     <div 
                     className="flex justify-between items-center gap-x-4"
                     >
-                        <>
-                          <div>
-                            <Button
-                                onClick={() => {}}
-                                className="
-                                bg-transparent
-                                text-neutral-300 
-                                font-medium
-                                "
-                            >
-                                Sign up
-                            </Button>
-                          </div>
-                          <div>
-                            <Button
-                                onClick={() => {}}
-                                className="
-                                bg-white px-6 py-2 
-                                "
-                            >
-                                Log in
-                            </Button>
-                          </div>
-                        </>
+                        {user ? (
+                            <div className="flex gap-x-4 items-center">
+                                <Button
+                                 onClick={handleLogout}
+                                 className="bg-white px-6 py-2"
+                                >
+                                    Logout
+                                </Button>
+                                <Button
+                                 onClick={() => router.push('/account')}
+                                 className="bg-white"
+                                >
+                                    <FaUserAlt />
+                                </Button>
+                            </div>
+                        ) : (
+                            <>
+                                <div>
+                                    <Button
+                                        onClick={authModal.onOpen}
+                                        className="
+                                        bg-transparent
+                                        text-neutral-300 
+                                        font-medium
+                                        "
+                                    >
+                                        Sign up
+                                    </Button>
+                                </div>
+                                <div>
+                                    <Button
+                                        onClick={authModal.onOpen}
+                                        className="
+                                        bg-white px-6 py-2 
+                                        "
+                                    >
+                                        Log in
+                                    </Button>
+                                </div>
+                            </>
+                        )}
                     </div>
             </div>
             {children}
